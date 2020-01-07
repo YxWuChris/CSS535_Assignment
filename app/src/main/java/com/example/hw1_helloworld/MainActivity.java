1be4e1a64a52c736759884f0b768c0666ff297f6
YxWuChris <zy18483@nottingham.edu.cn>
2020-01-07 15:06:13 -0800

    Add next page jump

--- app/src/main/java/com/example/hw1_helloworld/MainActivity.java
+++ app/src/main/java/com/example/hw1_helloworld/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.hw1_helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.next);
     }
+
+    public void nextPage(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/hw1_helloworld/SecondActivity.java
+++ app/src/main/java/com/example/hw1_helloworld/SecondActivity.java
@@ -1,14 +1,17 @@
 package com.example.hw1_helloworld;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        btn2 = (Button) findViewById(R.id.next);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_page);
     }
 }
