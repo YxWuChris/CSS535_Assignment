fd34d621f2d63d04c773632d27a1696a63f2fc3d
YxWuChris <zy18483@nottingham.edu.cn>
2020-01-08 00:51:06 -0800

    fix bug in manifest

--- app/src/main/java/com/example/hw1_helloworld/SecondActivity.java
+++ app/src/main/java/com/example/hw1_helloworld/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.example.hw1_helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,4 +16,10 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_page);
     }
+
+    public void backPage(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
